b9c8e45510a49aca0f3739403990629a6f605646
Angie Green <angie@greenorleans.com>
2015-03-22 14:01:07 -0500

    Add code for >600px and <200px images

--- app/controllers/pg_controller.rb
+++ app/controllers/pg_controller.rb
@@ -7,19 +7,20 @@ class PgController < ApplicationController
     @w = params[:w]
     @h = params[:h]
 
-    until @w.to_i % 20 == 0
-      @w = @w.to_i - 1
+    if @w.to_i <199 || @h.to_i <199
+      @size = "150x150"
+    elsif @w.to_i >619 || @h.to_i >619
+      @size = "800x800"
+    else
+      until @w.to_i % 20 == 0
+        @w = @w.to_i - 1
+      end
+      until @h.to_i % 20 == 0
+        @h = @h.to_i - 1
+      end
+        width = @w
+        height = @h
+        @size = width.to_s + 'x' + height.to_s
     end
-
-    until @h.to_i % 20 == 0
-      @h = @h.to_i - 1
-    end
-
-    width = @w
-    height = @h
-    @size = width.to_s + 'x' + height.to_s
-
   end
 end
-
-# What happens if it's smaller than 200 and larger than 600?
